869d47ec6e3b1f0f991c5afe5b98b9e2a6986a04
±è¿ëÀç <yjkim@unieye.co.kr>
2022-11-17 17:51:19 +0900

    git ignore 추가

---
+++ copy.bat
@@ -0,0 +1,6 @@
+xcopy U:\Project\UniEye3Wpf\UniEye3Wpf-master\Source\Application\UniscanC\WPF.UniscanCM\bin\x64\Release\*.* C:\UniScanCM\Bin\*.* /e /h /y
+xcopy U:\Keep\*.* C:\UniScanCM\Bin\*.* /e /h /y
+pause
+xcopy U:\Project\UniEye3Wpf\UniEye3Wpf-master\Source\Application\UniscanC\WPF.UniscanIM\bin\x64\Release\*.* C:\UniScanIM\Bin\*.* /e /h /y
+xcopy U:\Keep\*.* C:\UniScanCM\Bin\*.* C:\UniScanIM\Bin\*.* /e /h /y
+pause
